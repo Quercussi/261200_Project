91c1bb62fc7898efd6cee6f6c43aeec90366254a
FrienDDDD <friendjunk@gmail.com>
2023-03-12 20:14:07 +0700

    add Territory

---
+++ src/front-end/src/components/Territory.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HexagonGrid from "./hexagongrid";
+import times from "lodash/times";
+
+const Territory = () => {
+  const getHexProps = (hexagon) => {
+    return {
+      style: {
+        stroke: "black",
+      },
+    };
+  };
+
+  let rows = times(100, (id) => id);
+
+  return (
+    <HexagonGrid
+      gridWidth={450}
+      gridHeight={450}
+      hexagons={rows}
+      hexProps={getHexProps}
+    />
+  );
+};
+
+export default Territory;
